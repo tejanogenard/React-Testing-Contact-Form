6d17a44d8f8c8a20b3bd0e4164f7847b7cf1934a
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-03-12 14:19:27 -0700

    App set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import ContactForm from "./components/ContactForm";
 
 export default function App() {
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,33 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm"
 
 test("renders App without crashing", () => {
+  //Arrange 
   render(<App />);
+
+
+
 });
+  // testing the testing test
+  test("firstname, lastname, email, message inputs are rendered", () => {
+
+    //Arrange
+    const { getByLabelText } = render(<ContactForm  />)
+    getByLabelText(/firstName/i)
+  
+    //Act
+
+});
+
+
+/*
+    Create tests for 
+      * First Name input
+      * Last Name input 
+      * Email input 
+      * Message input
+  *handleSubmit ?? 
+
+*/ 
